e26c10d54a839013ebd31ebeadbf4ba08c416f57
Gabriel Arruda <gabrielalvesdearruda@gmail.com>
2021-11-11 11:31:36 -0300

    sounds

--- Assets/Scripts/ChangeLookAtTarget.cs
+++ Assets/Scripts/ChangeLookAtTarget.cs
@@ -5,6 +5,8 @@ public class ChangeLookAtTarget : MonoBehaviour {
 
 	public GameObject target; // the target that the camera should look at
 
+	private AudioSource[] listSounds;
+
 	void Start() {
 		if (target == null) 
 		{
@@ -17,6 +19,23 @@ public class ChangeLookAtTarget : MonoBehaviour {
 	void OnMouseDown () {
 		// change the target of the LookAtTarget script to be this gameobject.
 		LookAtTarget.target = target;
-		Camera.main.fieldOfView = 60*target.transform.localScale.x;
+
+		StopSounds();
+		// toca a música do planete clickado
+		target.GetComponent<AudioSource>().Play();
+
+		// ajusta o zoom
+		if(target.transform.localScale.x > 1.5) 
+			Camera.main.fieldOfView = 20*target.transform.localScale.x;
+		else				
+			Camera.main.fieldOfView = 60*target.transform.localScale.x;
 	}
+
+	// Para todas músicas
+	void StopSounds() {
+        listSounds = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+        foreach( AudioSource audioS in listSounds) {
+            audioS.Stop();
+        }
+    }
 }
